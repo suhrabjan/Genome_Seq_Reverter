ebf8c62ac591b84109e5548a6a7fd445f6c05aa2
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-09-23 13:38:13 -0500

    Sets up the initial DOM content, size, and positioning

---
+++ reverseComplement.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('bgd-anime');
+const body = document.querySelector('body')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.setAttribute('style', 'background-color: silver;')
+const ctx = canvas.getContext('2d');
+
+
